e59be99b6b50e045c225716bda3a8faf9fed3350
garikbesson <garikbesson@gmail.com>
2024-12-19 17:34:31 +0100

    add pre-built wasm files

--- tests/init.rs
+++ tests/init.rs
@@ -1,6 +1,50 @@
+use std::sync::LazyLock;
+
 use near_contract_standards::non_fungible_token::TokenId;
 use near_sdk::{serde_json::json, NearToken};
 use near_workspaces::{Account, Contract, DevNetwork, Worker};
+
+static ROOT_CONTRACT_WASM: LazyLock<Vec<u8>> = LazyLock::new(|| {
+    let artifact_path = "tests/contracts/root/res/root.wasm";
+
+    let contract_wasm = std::fs::read(artifact_path).expect(
+        format!(
+            "Could not read Root contract WASM file from {}",
+            artifact_path
+        )
+        .as_str(),
+    );
+
+    contract_wasm
+});
+
+static FT_CONTRACT_WASM: LazyLock<Vec<u8>> = LazyLock::new(|| {
+    let artifact_path = "tests/contracts/ft/res/fungible_token.wasm";
+
+    let contract_wasm = std::fs::read(artifact_path).expect(
+        format!(
+            "Could not read Fungible token WASM file from {}",
+            artifact_path
+        )
+        .as_str(),
+    );
+
+    contract_wasm
+});
+
+static NFT_CONTRACT_WASM: LazyLock<Vec<u8>> = LazyLock::new(|| {
+    let artifact_path = "tests/contracts/nft/res/non_fungible_token.wasm";
+
+    let contract_wasm = std::fs::read(artifact_path).expect(
+        format!(
+            "Could not read Non-fungible token WASM file from {}",
+            artifact_path
+        )
+        .as_str(),
+    );
+
+    contract_wasm
+});
 
 pub async fn init(
     worker: &Worker<impl DevNetwork>,
@@ -9,8 +53,7 @@ pub async fn init(
     let wasm = near_workspaces::compile_project(".").await?;
     let contract = worker.dev_deploy(&wasm).await?;
 
-    let root_contract = near_workspaces::compile_project("./tests/contracts/root").await?;
-    let _ = root.deploy(&root_contract).await?;
+    let _ = root.deploy(&ROOT_CONTRACT_WASM).await?;
 
     let creator = root
         .create_subaccount("creator")
@@ -35,8 +78,7 @@ pub async fn init_ft_contract(
     worker: &Worker<impl DevNetwork>,
     creator: &Account,
 ) -> anyhow::Result<Contract> {
-    let ft_wasm = near_workspaces::compile_project("./tests/contracts/ft").await?;
-    let ft_contract = worker.dev_deploy(&ft_wasm).await?;
+    let ft_contract = worker.dev_deploy(&FT_CONTRACT_WASM).await?;
 
     let res = ft_contract
         .call("new_default_meta")
@@ -53,8 +95,7 @@ pub async fn init_nft_contract(
     worker: &Worker<impl DevNetwork>,
     creator: &Account,
 ) -> anyhow::Result<(Contract, TokenId)> {
-    let nft_wasm = near_workspaces::compile_project("./tests/contracts/nft").await?;
-    let nft_contract = worker.dev_deploy(&nft_wasm).await?;
+    let nft_contract = worker.dev_deploy(&NFT_CONTRACT_WASM).await?;
 
     let new_default_res = nft_contract
         .call("new_default_meta")
